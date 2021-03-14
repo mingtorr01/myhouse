30c9bf806eec833c7e598d90f5507f9f49c435b4
mingtorr <mingtorr@gmail.com>
2021-03-15 02:31:04 +0900

    mongoose 연결실패

--- server/routes/mongod.js
+++ server/routes/mongod.js
@@ -12,6 +12,7 @@ mongoose.connect(uri, {
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 
 db.on("error", console.error);
@@ -20,8 +21,39 @@ db.once("open", function () {
   console.log("Connected to mongod server");
 });
 
-router.get("/message", function (req, res, next) {
-  res.send(process.env.MESSAGE);
+// 찾기명령
+db.collection("녹지비율").findOne({ _id: "604e0d6249f0520cb073fb48" }, function (err, data) {
+  console.log(data);
+});
+
+// User 전체 조회
+router.get("/", function (req, res) {
+  User.find({}, function (err, users) {
+    if (err) return res.status(500).send("User 전체 조회 실패.");
+    res.status(200).send(users);
+  });
+});
+// User 조회
+router.get("/:id", function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    if (err) return res.status(500).send("User 조회 실패");
+    if (!user) return res.status(404).send("User 없음.");
+    res.status(200).send(user);
+  });
+});
+// User 삭제
+router.delete("/:id", function (req, res) {
+  User.findByIdAndRemove(req.params.id, function (err, user) {
+    if (err) return res.status(500).send("User 삭제 실패");
+    res.status(200).send("User " + user.name + " 삭제됨.");
+  });
+});
+// User 수정
+router.put("/:id", function (req, res) {
+  User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
+    if (err) return res.status(500).send("User 수정 실패.");
+    res.status(200).send(user);
+  });
 });
 
 module.exports = router;
