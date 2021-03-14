832378b2faad00c67a7a4dfc4a33e21ac07f4021
mingtorr <mingtorr@gmail.com>
2021-03-14 17:28:09 +0900

    server mod

--- server/routes/mongod.js
+++ server/routes/mongod.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false });
-var db = mongoose.connection;
+const uri = process.env.ATLAS_URI; // mongoDB Connect 정보
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+console.log("config", config.mongoURI);
+/*
 db.on("error", console.error);
 db.once("open", function () {
   // 몽고디비 서버에 연결
@@ -11,5 +18,5 @@ db.once("open", function () {
 });
 
 router.get("/");
-
+*/
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const port = process.env.PORT || 3001;
 const cors = require("cors");
 
 app.use(express.json()); //bodyparser 역할 없으면 req.body 안먹힌다.
-app.use(cors); // 서버와 클라이언트 사이의 크로스 도메인 헤더 라이브러리
+app.use(cors()); // 서버와 클라이언트 사이의 크로스 도메인 헤더 라이브러리
 
-const route = require("./routes/indexs");
-app.use("/api", route); //라우팅
+const route = require("./routes/mongod");
+app.use("/db", route); //라우팅
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
